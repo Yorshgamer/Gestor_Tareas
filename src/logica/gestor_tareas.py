338149787605e62d35d08724587ea3c79feac86f
Yorshgamer <75707785@continental.edu.pe>
2024-06-03 15:22:36 -0500

    Configuración inicial del proyecto Gestor de tareas

---
+++ src/logica/gestor_tareas.py
@@ -0,0 +1,7 @@
+class Tarea:
+    def __init__(self, titulo, descripcion):
+        self.titulo = titulo
+        self.descripcion = descripcion
+        self.completada = False
+class GestorTareas:
+    pass
